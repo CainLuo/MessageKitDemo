4f5db99caad6cd46826390148e7b9b1c0bafcc7c
CainLuo <350116542@qq.com>
2021-02-24 00:23:53 +0800

    Update ChatViewController+MessagesDataSource.swift

--- MessageKitDemo/Basic/ChatViewController+MessagesDataSource.swift
+++ MessageKitDemo/Basic/ChatViewController+MessagesDataSource.swift
@@ -13,14 +13,17 @@ extension ChatViewController: MessagesDataSource {
         return SampleData.shared.currentSender
     }
     
+    // 返回消息的数量
     func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
         return messageList.count
     }
     
+    // 返回消息类型
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
         return messageList[indexPath.section]
     }
     
+    // 返回分割的日期
     func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         if indexPath.section % 3 == 0 {
             return NSAttributedString(string: MessageKitDateFormatter.shared.string(from: message.sentDate),
@@ -30,21 +33,24 @@ extension ChatViewController: MessagesDataSource {
         return nil
     }
     
+    // 返回是否已读
     func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         
 //        return NSAttributedString(string: "Read", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10), NSAttributedString.Key.foregroundColor: UIColor.darkGray])
         nil
     }
     
+    // 返回姓名
     func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         let name = message.sender.displayName
         return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
     }
     
+    // 返回日期
     func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         
-//        let dateString = formatter.string(from: message.sentDate)
-//        return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
-        nil
+        let dateString = formatter.string(from: message.sentDate)
+        return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
+//        nil
     }
 }
