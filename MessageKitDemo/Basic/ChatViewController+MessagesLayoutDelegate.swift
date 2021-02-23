a178e3ed2501de2a878e206780d849e08b980c1c
CainLuo <350116542@qq.com>
2021-02-24 00:23:50 +0800

    Update ChatViewController+MessagesLayoutDelegate.swift

--- MessageKitDemo/Basic/ChatViewController+MessagesLayoutDelegate.swift
+++ MessageKitDemo/Basic/ChatViewController+MessagesLayoutDelegate.swift
@@ -11,19 +11,23 @@ import MessageKit
 // MARK: - MessagesLayoutDelegate
 extension ChatViewController: MessagesLayoutDelegate {
     
+    // 显示分割时间戳的高度
     func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 18
     }
     
+    // 显示已读
     func cellBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 0
+        return 17
     }
     
+    // 名称的高度
     func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 20
     }
     
+    // 显示日期的高度
     func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
-        return 0
+        return 16
     }
 }
